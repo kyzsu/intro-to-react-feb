28f5466e2af2e737b40c187bad24af5dd50e7a59
Immanuel W <l0calh34rtz@gmail.com>
2025-02-17 10:47:34 +0700

    feat: add vite

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
 const Pizza = (props) => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, props.nama_pizza),
