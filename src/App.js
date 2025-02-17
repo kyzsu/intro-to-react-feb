afdaf74ed1bf46916991303a2cf260070f28efc4
Immanuel W <l0calh34rtz@gmail.com>
2025-02-17 09:43:46 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+const Pizza = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, props.nama_pizza),
+    React.createElement("p", {}, props.deskripsi),
+  ]);
+};
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Best Pizza in Jakarta!"),
+    React.createElement(Pizza, {
+      nama_pizza: "Meat Lovers",
+      deskripsi: "Daging sapi, ayam, sosis.",
+    }),
+    React.createElement(Pizza, {
+      nama_pizza: "vegetarian",
+      deskripsi: "sawi, jamur, jagung",
+    }),
+    React.createElement(Pizza, {
+      nama_pizza: "Pepperoni",
+      deskripsi: "Saus Tomat, Pepperoni, Keju",
+    }),
+  ]);
+};
+
+const container = document.getElementById("root");
+const root = ReactDOM.createRoot(container);
+root.render(React.createElement(App));
